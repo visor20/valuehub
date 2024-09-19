ebfd97f161cb1c4469a48b2c7380a94fa641de61
RichardViso <richardnviso@gmail.com>
2024-09-19 13:50:59 -0500

    add general company info

--- src/main.py
+++ src/main.py
@@ -19,8 +19,8 @@ def get_openinsider():
     start = end - timedelta(days=settings.LOOKBACK)
     start_str = start.strftime('%m/%d/%Y')
     end_str = end.strftime('%m/%d/%Y')
+    print("start - end: " + start_str + "-" + end_str + '\n')
 
-    print("start - end: " + start_str + "-" + end_str)
     url = f'http://openinsider.com/screener?s=&o=&pl=&ph=&ll=&lh=&fd=-1&fdr={start_str}+-+{end_str}&td=0&tdr=&fdlyl=&fdlyh=&daysago=&xp=1&xs=1&vl=&vh=&ocl=&och=&sic1=-1&sicl=100&sich=9999&grp=0&nfl=&nfh=&nil=&nih=&nol=&noh=&v2l=&v2h=&oc2l=&oc2h=&sortcol=0&cnt=5000&page=1'
    
     response = requests.get(url)
@@ -29,8 +29,7 @@ def get_openinsider():
     try:
         rows = soup.find('table', {'class': 'tinytable'}).find('tbody').findAll('tr')
     except:
-        print("Error! Skipping")
-        logger.error(f"This URL was not successful: {url}")
+        print("URL ERROR")
         return
 
     for row in rows:
@@ -70,20 +69,33 @@ def main():
     drop_columns(data)
     ticker_dict = get_ticker_dict(data)
     
-    df = pd.DataFrame(columns=['ticker', 'num_trades', 'P/E', 'PEG', 'P/B'])
+    df = pd.DataFrame(columns=['ticker', 'sector', 'price', 'DCF', 'market_cap', 'num_trades', 'P/E', 'PEG', 'P/B'])
     for t in ticker_dict.items():
         if t[1] >= settings.CLUSTER_VAL:
-            url = f'https://financialmodelingprep.com/api/v3/ratios-ttm/{t[0]}?apikey={api_key}'
-            response = requests.get(url)
-            t_data = response.json()
+            # TTM == trailing twelve months
+            ratios_url = f'https://financialmodelingprep.com/api/v3/ratios-ttm/{t[0]}?apikey={api_key}'
+            profile_url = f'https://financialmodelingprep.com/api/v3/profile/{t[0]}?apikey={api_key}'
 
-            # TTM = trailing twelve months
-            pe, pb, peg = 0, 0, 0
-            if t_data:
-                pe = t_data[0]['peRatioTTM']
-                pb = t_data[0]['priceToBookRatioTTM']
-                peg = t_data[0]['pegRatioTTM']
-            df.loc[len(df)] = {'ticker': t[0], 'num_trades': t[1], 'P/E': pe, 'PEG': peg, 'P/B': pb}
+            ratios_response = requests.get(ratios_url)
+            profile_response = requests.get(profile_url)
+            r_data = ratios_response.json()
+            p_data = profile_response.json()
+
+            sector, price, dcf, mkt_cap, pe, pb, peg = 0, 0, 0, 0, 0, 0, 0
+            if r_data:
+                pe = r_data[0]['peRatioTTM']
+                pb = r_data[0]['priceToBookRatioTTM']
+                peg = r_data[0]['pegRatioTTM']
+
+            if p_data:
+                sector = p_data[0]['sector']
+                price = p_data[0]['price']
+                dcf = p_data[0]['dcf']
+                mkt_cap = p_data[0]['mktCap']
+
+            df.loc[len(df)] = ({'ticker': t[0], 'sector': sector,
+                                'price': price, 'DCF': dcf, 'market_cap': mkt_cap, 
+                                'num_trades': t[1], 'P/E': pe, 'PEG': peg, 'P/B': pb})
 
     sorted_df = df.sort_values(by='P/E')
     print(sorted_df.reset_index(drop=True))
--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,4 @@
 
 CLUSTER_VAL = 4
-LOOKBACK = 21
+LOOKBACK = 5
 KEYS = ['X', 'filing_date', 'trade_date', 'ticker', 'company_name', 'owner_name', 'Title', 'trade_type', 'last_price', 'Qty', 'Owned', 'delta_owned', 'Value']
